c082594ea817f79bacb45879169cc1de52257d59
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-09-29 15:33:21 -0400

    [feat] Setup CLI parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::App::new("vshlog")
+        .version("0.1")
+        .author("Bryan Bugyi <bryanbugyi34@gmail.com>")
+        .about("\nView your Shell History with Vim")
+        .arg(
+            clap::Arg::with_name("verbose")
+            .short("v")
+            .long("verbose")
+            .multiple(true)
+            .help("Increase verbosity level. Can be used multiple times.")
+        )
+        .get_matches();
 }
